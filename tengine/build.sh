8e566742d7b237bfe53aa0780ff7e6bb578b125e
eduosi <webmaster@buession.com>
2019-04-18 19:53:05 +0800

    add tengine build.sh

---
+++ tengine/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TENGINE_VERSION=2.2.3
+
+docker build --force-rm -t eduosi/tengine:$TENGINE_VERSION .
+docker build --force-rm -t eduosi/tengine:latest .
+
+docker tag eduosi/tengine:$TENGINE_VERSION eduosi/tengine:$TENGINE_VERSION
+docker tag eduosi/tengine:latest eduosi/tengine:latest
+
+docker push eduosi/tengine:$TENGINE_VERSION
+docker push eduosi/tengine:latest
