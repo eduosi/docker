f32fc4c7bc14551cf4432d6805217db9b363ed61
eduosi <webmaster@buession.com>
2019-05-08 23:44:32 +0800

    update

--- elasticsearch/build.sh
+++ elasticsearch/build.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-ELASTICSEARCH_VERSION=6.5.4
+ELASTICSEARCH_VERSION=7.0.1
 
 docker build --force-rm -t eduosi/elasticsearch:$ELASTICSEARCH_VERSION .
 docker build --force-rm -t eduosi/elasticsearch:latest .
+docker build --force-rm -t omtech/elasticsearch:$ELASTICSEARCH_VERSION .
+docker build --force-rm -t omtech/elasticsearch:latest .
 
 docker tag eduosi/elasticsearch:$ELASTICSEARCH_VERSION eduosi/elasticsearch:$ELASTICSEARCH_VERSION
 docker tag eduosi/elasticsearch:latest eduosi/elasticsearch:latest
+docker tag omtech/elasticsearch:$ELASTICSEARCH_VERSION reg.omtech.cn/omtech/elasticsearch:$ELASTICSEARCH_VERSION
+docker tag omtech/elasticsearch:latest reg.omtech.cn/omtech/elasticsearch:latest
 
 #docker push eduosi/elasticsearch:$ELASTICSEARCH_VERSION
 #docker push eduosi/elasticsearch:latest
+docker push reg.omtech.cn/omtech/elasticsearch:$ELASTICSEARCH_VERSION
+docker push reg.omtech.cn/omtech/elasticsearch:latest
